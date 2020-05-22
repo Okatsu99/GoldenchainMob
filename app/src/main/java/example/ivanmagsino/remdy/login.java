4ddac2f265b73c8535f515bc841023beadf83c57
Ivan Magsino <201801483@iacademy.edu.ph>
2020-05-22 19:43:24 +0800

    Revert "added loading login and patient record and inventory activity"
    This reverts commit 99053650a875523520681c46337422d2b61fe257.

---
+++ app/src/main/java/example/ivanmagsino/remdy/login.java
@@ -0,0 +1,28 @@
+package example.ivanmagsino.remdy;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void Login(){
+        Intent intent=new Intent(login.this, index.class);
+        startActivity(intent);
+    }
+
+}
