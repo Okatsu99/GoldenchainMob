99053650a875523520681c46337422d2b61fe257
Ivan Magsino <201801483@iacademy.edu.ph>
2020-05-22 19:36:50 +0800

    added loading login and patient record and inventory activity

---
+++ ReMDy3/app/src/main/java/example/ivanmagsino/remdy/MainActivity.java
@@ -0,0 +1,47 @@
+package example.ivanmagsino.remdy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ImageView appointmentbtn = findViewById(R.id.imgAppoint);
+        ImageView patientrecordbtn = findViewById(R.id.imgRecord);
+        ImageView inventorybtn = findViewById(R.id.imgIventory);
+
+        appointmentbtn.setOnClickListener(this);
+        patientrecordbtn.setOnClickListener(this);
+        inventorybtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgAppoint:
+                Intent appointActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(appointActivity);
+                break;
+
+            case R.id.imgRecord:
+                Intent patientActivity = new Intent(getApplicationContext(), inventory.class);
+                startActivity(patientActivity);
+                break;
+
+            case R.id.imgIventory:
+                Intent inventActivity = new Intent(getApplicationContext(), patientrecord.class);
+                startActivity(inventActivity);
+                break;
+        }
+    }
+}
---
+++ ReMDy3/app/src/main/java/example/ivanmagsino/remdy/patientrecord.java
@@ -0,0 +1,33 @@
+package example.ivanmagsino.remdy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class patientrecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+
+        Button addRecord = findViewById(R.id.btnAddRecord);
+        Button editRecord = findViewById(R.id.btnEditRecord);
+        Button deleteRecord = findViewById(R.id.btnDeleteRecord);
+
+    }
+
+    public void addRecord(View v){
+
+    }
+
+    public void editRecord(View v){
+
+    }
+
+    public void deleteRecord(View v){
+
+    }
+}
